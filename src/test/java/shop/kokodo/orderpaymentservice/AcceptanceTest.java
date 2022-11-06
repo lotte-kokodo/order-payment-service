0b53999b712301cf64ff4ceab801873ad621e549
SSOsh <tngh147258@naver.com>
2022-11-06 20:46:48 +0900

    Test : restassured를 위한 테스트 코드 작성 진행중

---
+++ src/test/java/shop/kokodo/orderpaymentservice/AcceptanceTest.java
@@ -0,0 +1,33 @@
+package shop.kokodo.orderpaymentservice;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+
+/**
+ * packageName    : order-payment-service
+ * fileName       : AcceptanceTest
+ * author         : tngh1
+ * date           : 2022-11-05
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2022-11-05        tngh1              최초 생성
+ */
+@ActiveProfiles("test")
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public abstract class AcceptanceTest {
+
+    @LocalServerPort
+    int port;
+
+    @BeforeEach
+    public void setUp() {
+        RestAssured.port = port;
+    }
+}
